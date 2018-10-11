e4655f6d7c0f8ed45b2f1c8851663aa04530084a
weizai133 <31020832+weizai133@users.noreply.github.com>
2018-10-11 14:57:59 +1300

    From Raymond -Updating Main Menu

--- app/src/main/java/com/treeshopapp/treeshop/Mainmenu.java
+++ app/src/main/java/com/treeshopapp/treeshop/Mainmenu.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.treeshopapp.treeshopapp.MainActivity;
@@ -61,6 +62,8 @@ public class Mainmenu extends AppCompatActivity implements View.OnClickListener 
         if(ContextCompat.checkSelfPermission(Mainmenu.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
 
         }
+
+
 
     }
 
